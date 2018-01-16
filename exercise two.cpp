480ab395446bc274944ac453c6ae73e3356b1d23
fizfiq <33883958+fizfiq@users.noreply.github.com>
2018-01-16 10:27:36 +0800

    Add files via upload

---
+++ exercise two.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int sum(int x,int y);
+
+int main()
+{
+	int x=3,y=4;
+	sum ( x, y );
+	
+	
+}
+int sum(int x,int y)
+{
+	int result;
+	result = x+y;
+	
+	cout<<result;
+
+}
