4d3d3245df377977291cb8e58df0d83ee10bb8a7
fizfiq <33883958+fizfiq@users.noreply.github.com>
2018-01-16 11:02:38 +0800

    Add files via upload

---
+++ exercise one.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void masuk_tak();
+
+int main()
+{
+   masuk_tak();
+}
+
+void masuk_tak()
+{
+	char ans;
+	int x=0;
+	cout<<endl<<"Dia shoot masuk tak? (answer y-yes atau n-tidak"<<endl;
+	cin>>ans;
+
+	//ans=' ';
+	while(ans!='y' && ans!='y')
+	{
+		cout<<endl<<"cakap lah bebetul, try again: "<<endl;
+		cin>>ans;
+		x++;
+		if(x==3)
+			break;
+		if(ans!='n' && ans!='n')
+			break;
+		}
+
+	if(ans=='n')
+	{
+		cout<<endl<<"the fudge?!";
+	//	masuk_tak();
+	}
+	if(ans=='y')
+	cout<<endl<<"chantek!"<<endl;
+}
+
